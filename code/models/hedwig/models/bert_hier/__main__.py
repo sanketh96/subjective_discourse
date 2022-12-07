c8b195d14962ad3823b2f8c98c09b5e05a0f7368
Priya Bagaria <pbagaria@andrew.cmu.edu>
2022-12-07 17:04:32 -0500

    Sorted by min of logits from teacher model

--- code/models/hedwig/models/bert_hier/__main__.py
+++ code/models/hedwig/models/bert_hier/__main__.py
@@ -10,6 +10,7 @@ from common.evaluators.bert_hierarchical_evaluator import BertHierarchicalEvalua
 from common.trainers.bert_hierarchical_trainer import BertHierarchicalTrainer
 from datasets.bert_processors.congressional_hearing_processor import CongressionalHearingProcessor
 from datasets.bert_processors.congressional_hearing_explanations_processor import CongressionalHearingExplanationsProcessor
+from datasets.bert_processors.congressional_hearing_logits_sorted_processor import CongressionalHearingLogitsSortedProcessor
 from models.bert_hier.args import get_args
 from models.bert_hier.model import BertHierarchical, RobertaHierarchical, XLNetHierarchical, DebertaHierarchical
 
@@ -72,7 +73,8 @@ def run_main(args):
 
     dataset_map = {
         'CongressionalHearing': CongressionalHearingProcessor,
-        'CongressionalHearingExplanations': CongressionalHearingExplanationsProcessor
+        'CongressionalHearingExplanations': CongressionalHearingExplanationsProcessor,
+        'CongressionalHearingLogitsSorted': CongressionalHearingLogitsSortedProcessor
     }
 
     tokenizer_map = {
